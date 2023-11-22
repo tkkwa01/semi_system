5e37ffe99d2aced939c0eff378f1de404ee30cbe
tkkwa01 <aoisyanok@gmail.com>
2023-11-22 14:03:05 +0900

    implement keijiban user presenter

--- keijiban/adopter/presenter/user.go
+++ keijiban/adopter/presenter/user.go
@@ -2,6 +2,7 @@ package presenter
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"semi_systems/keijiban/domain"
 	"semi_systems/keijiban/usecase"
 )
@@ -19,26 +20,26 @@ func NewUserOutputFactory() UserOutputFactory {
 }
 
 func (u user) CreateUser(id uint) error {
-	//TODO implement me
-	panic("implement me")
+	u.c.JSON(http.StatusCreated, gin.H{"id": id})
+	return nil
 }
 
 func (u user) GetAllUser(res []*domain.User) error {
-	//TODO implement me
-	panic("implement me")
+	u.c.JSON(http.StatusOK, res)
+	return nil
 }
 
 func (u user) GetUserByID(res domain.User) error {
-	//TODO implement me
-	panic("implement me")
+	u.c.JSON(http.StatusOK, res)
+	return nil
 }
 
 func (u user) UpdateUser(res domain.User) error {
-	//TODO implement me
-	panic("implement me")
+	u.c.JSON(http.StatusOK, res)
+	return nil
 }
 
 func (u user) DeleteUser() error {
-	//TODO implement me
-	panic("implement me")
+	u.c.JSON(http.StatusOK, "")
+	return nil
 }
